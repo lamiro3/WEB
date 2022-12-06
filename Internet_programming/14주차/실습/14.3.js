0e2cc4d4718c1b6939e3c3ef20529bc43ead1972
lamiro3 <lamiro3@naver.com>
2022-12-06 14:11:50 +0900

    Internet programming last assignment done

--- Internet_programming/14주차/실습/14.3.js
+++ Internet_programming/14주차/실습/14.3.js
@@ -2,18 +2,18 @@ var num = new Array(0, 0, 0);
 var sen = document.querySelectorAll('.num');
 var msg = document.querySelector('#msg');
 
-function getRandomInt(min, max) {
+function getRandomInt(min, max) { // min과 max 사이의 랜덤한 정수를 return하는 함수
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function gamble(emt, idx){
-    num[idx] = getRandomInt(0, 2);
+    num[idx] = getRandomInt(0, 3);
     emt.innerText = num[idx];
 
-    if (idx == 2) {
-        if (num[0] == num[1] && num[2] == num[1])
+    if (idx == 2) { // 세 번째 숫자를 눌렀을 경우
+        if (num[0] == num[1] && num[2] == num[1]) // 모든 숫자가 같을 때
             msg.innerText = '성공입니다!!';
         else
             msg.innerText = '실패입니다..';
